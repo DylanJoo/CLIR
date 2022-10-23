776565bcac2cabde4a5797cef33a80fb370faf25
DylanJoo <dylanjootw@gmail.com>
2022-10-23 15:27:06 +0800

    [reranking] model variants

---
+++ run_evaluation.sh
@@ -0,0 +1,38 @@
+# hc4
+for split in dev test;do
+    echo '|----------|--------|---------|---------|------|---------|---------|---------|--------|'
+    echo '| Language | Source | Ranking |   R@100 | R@1K | nDCG@20 | mAP@20  | mAP@100 | mAP@1K |'
+    echo '|----------|--------|---------|---------|------|---------|---------|---------|--------|'
+    for lang in fas rus zho;do
+        for source in human;do
+        # for source in human google nllb2 sockeye2;do
+            FILE=runs/${lang}/hc4.${lang}.${split}.${source}.td.spr.top1000.trec
+            echo  $lang '|' $source '| spr |' 
+            ./tools/trec_eval-9.0.7/trec_eval \
+                -m ndcg_cut.20 \
+                -m map_cut.20,100,1000 \
+                -m recall.100,1000 \
+                data/hc4/${lang}/${split}.qrels.v1-0.txt $FILE | cut -f3 | sed ':a; N; $!ba; s/\n/|/g'
+
+            # monomt5 
+            FILE=runs/${lang}/hc4.${lang}.${split}.${source}.monomT5.trec
+            echo  $lang '|' $source '| monomt5 |' 
+            ./tools/trec_eval-9.0.7/trec_eval \
+                -m ndcg_cut.20 \
+                -m map_cut.20,100,1000 \
+                -m recall.100,1000 \
+                data/hc4/${lang}/${split}.qrels.v1-0.txt $FILE | cut -f3 | sed ':a; N; $!ba; s/\n/|/g'
+
+            # # monomt5 - dq
+            # FILE=runs/${lang}/hc4.${lang}.${split}.${source}.monomT5-dq.trec
+            # echo  $lang '|' $source '| monomt5-dq |' 
+            # ./tools/trec_eval-9.0.7/trec_eval \
+            #     -m ndcg_cut.20 \
+            #     -m map_cut.20,100,1000 \
+            #     -m recall.100,1000 \
+            #     data/hc4/${lang}/${split}.qrels.v1-0.txt $FILE | cut -f3 | sed ':a; N; $!ba; s/\n/|/g'
+
+            echo '|----------|--------|-------|------|---------|---------|--------|'
+        done
+    done
+done
---
+++ run_monomt5+.sh
@@ -0,0 +1,22 @@
+# baseline reranking model unicamp-dl/mt5-base-mmarco-v2
+for split in test dev;do
+    for lang in fas rus zho;do
+        for src in human;do
+            python3 passage_reranking.py \
+              --run runs/${lang}/hc4.${lang}.${split}.${src}.td.spr.top1000.trec \
+              --topic data/hc4/${split}.jsonl \
+              --collection /tmp2/trec/hc4/${lang}/hc4_docs_tc.jsonl \
+              --topk 1000 \
+              --output_trec runs/${lang}/hc4.${lang}.${split}.${src}.monomT5L-dq.trec \
+              --model_name_or_path checkpoints/mt5-large-mmarco-v2-temp \
+              --batch_size 4 \
+              --max_length 512 \
+              --gpu 1 \
+              --prefix monomt5-L-dq \
+              --lang ${lang} \
+              --src ${src}
+        done
+    done
+done
+              # --model_name_or_path unicamp-dl/mt5-base-mmarco-v2 \
+              # --model_name_or_path DylanJHJ/mt5-large-mmarco-v2-dq \
